8c9d2619b5aa9ad96d9ae20b57ff9fa1aede34a3
lellelasaikiran <lellelasaikiran@gmail.com>
2024-08-11 18:56:57 +0530

    Added styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   title = 'tictactoe';
   isPlayerA: boolean = true;
   isGameOver: boolean = false;
+  winnerDescription: string = '';
   box: string[][] = [
     ['', '', ''],
     ['', '', ''],
@@ -24,7 +25,7 @@ export class AppComponent {
       this.box[i][j] = this.box[i][j] == '' && this.isPlayerA ? 'X' : 'O';
       if (this.checkWinner(i, j) == 'won') {
         this.isGameOver = true;
-        console.log(`Player ${this.isPlayerA ? 'X' : '0'} won`);
+        this.winnerDescription = `Player ${this.isPlayerA ? 'X' : 'O'} won`;
       } else if (this.checkWinner(i, j) == 'tie') this.isGameOver = true;
       this.isPlayerA = !this.isPlayerA;
     }
@@ -33,25 +34,30 @@ export class AppComponent {
     const playerChar: string = this.isPlayerA ? 'X' : 'O';
     let rowChecker: boolean = true,
       columnChecker: boolean = true;
+    //rowChecker
     for (let i = 0; i < 3; i++) {
       if (playerChar != this.box[row][i]) {
         rowChecker = false;
         break;
       }
     }
+    //columnChecker
     for (let i = 0; i < 3; i++) {
       if (playerChar != this.box[i][col]) {
         columnChecker = false;
         break;
       }
     }
+    //main diag
     let diag1Win =
       row === col && this.box[0][0] === playerChar && this.box[1][1] === playerChar && this.box[2][2] === playerChar;
+    //anti-diag
     let diag2Win =
       row + col === 2 &&
       this.box[0][2] === playerChar &&
       this.box[1][1] === playerChar &&
       this.box[2][0] === playerChar;
+    //tie
     if (rowChecker || columnChecker || diag1Win || diag2Win) return 'won';
     let isTie = true;
     for (let i = 0; i < 3; i++) {
